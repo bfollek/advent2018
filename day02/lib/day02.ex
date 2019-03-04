b7ea52bf899323cf8c70742cfbadcdba9ad9f4ec
Bob Follek <bfollek@gmail.com>
2019-03-04 06:34:25 -0500

    WIP

--- day02/lib/day02.ex
+++ day02/lib/day02.ex
@@ -46,4 +46,38 @@ defmodule Day02 do
     |> Enum.filter(&Enum.any?(&1, fn letter_list -> length(letter_list) == len end))
     |> length
   end
+
+  @doc """
+  Read a file of strings;
+  Find the two strings that differ by one char in the same position;
+  Remove the differing char and return the common chars.
+
+  Conditions:
+  Preserve the original order of the comman chars.
+
+  ## Examples
+
+      iex> Day02.part2("data/day02.txt")
+      "fvstwblgqkhpuixdrnevmaycd"
+
+  """
+  def part2(file_name) do
+    ss =
+      File.stream!(file_name)
+      |> Stream.map(&String.trim/1)
+
+    # iex(34)> for e1 <- l1, e2 <- l2, do: [e1, e2]
+    # [[1, 4], [1, 5], [1, 6], [2, 4], [2, 5], [2, 6], [3, 4], [3, 5], [3, 6]]
+
+    # Permute ss with itself.
+    # From ["abc", "def", "ghi"] to [["abc", "def"], ["abc", "ghi"], ["def", "ghi"]]
+    pairs = for e1 <- ss, e2 <- ss, e1 != e2, do: [e1, e2]
+    IO.puts(inspect(pairs))
+
+    "?"
+    # iex(29)> Enum.zip([1,2,3], [1,2,3])
+    # [{1, 1}, {2, 2}, {3, 3}]
+    # iex(28)> String.myers_difference("abcd", "abzd")
+    # [eq: "ab", del: "c", ins: "z", eq: "d"]
+  end
 end
