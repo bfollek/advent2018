361f107af8f8336d1fc53891bc5bd7995d20db4f
Bob Follek <bfollek@gmail.com>
2019-03-18 21:33:53 -0400

    Progress, but there are things I don't like in my code.

--- day03/lib/day03.ex
+++ day03/lib/day03.ex
@@ -20,6 +20,7 @@ defmodule Day03 do
   """
   def part1(file_name) do
     claims = file_to_claims(file_name)
+    inches = Enum.reduce(claims, %{}, &map_inches/2)
     999
   end
 
@@ -31,7 +32,24 @@ defmodule Day03 do
       |> Stream.map(&String.trim/1)
       |> Stream.map(&Regex.run(re, &1))
       |> Enum.map(fn [_, id, left, top, width, height] ->
-        %Claim{id: id, left: left, top: top, width: width, height: height}
+        %Claim{
+          id: id,
+          left: String.to_integer(left),
+          top: String.to_integer(top),
+          width: String.to_integer(width),
+          height: String.to_integer(height)
+        }
+      end)
+    end
+
+    defp map_inches(claim, inches) do
+      elements =
+        for x <- claim.left..(claim.left + claim.width - 1),
+            y <- claim.top..(claim.top + claim.height - 1),
+            do: {{x, y}, [claim.id]}
+
+      Map.merge(inches, Map.new(elements), fn _k, v1, v2 ->
+        v1 ++ v2
       end)
     end
   end
